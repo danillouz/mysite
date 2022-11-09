6ec749f9135224a7db91b9f7eb7dbd3e2ce79079
Daniël Illouz <danillouz@users.noreply.github.com>
2022-11-09 22:16:48 +0100

    Inject post reading time via plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import mdx from "@astrojs/mdx"
 
+import { remarkReadingTime } from "./src/plugins/reading-time"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.danillouz.dev",
-  integrations: [sitemap(), tailwind(), mdx()],
+  integrations: [
+    sitemap(),
+    tailwind(),
+    mdx({
+      extendDefaultPlugins: true,
+      remarkPlugins: [remarkReadingTime],
+    }),
+  ],
 })
