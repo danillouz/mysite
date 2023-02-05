c1e5818b004cbd48630535533f6d600f9e3f6e01
Daniël Illouz <danillouz@users.noreply.github.com>
2023-02-05 12:35:54 +0100

    Add RSS feed for tags

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = "Dan's site"
 export const SITE_DESCRIPTION = "Welcome to my personal site!"
 export const FULL_NAME = "Daniël Illouz"
-export const RSS_TITLE = "Dan's posts"
+export const RSS_TITLE = "RSS feed for Dan's posts"
 export const RSS_DESCRIPTION =
   "Writing about things I learned and find interesting."
 
---
+++ src/pages/posts/tags/[tag].rss.ts
@@ -0,0 +1,57 @@
+import { getCollection } from "astro:content"
+import rss from "@astrojs/rss"
+import * as config from "@config"
+import { getAlphabeticallySortedTagsFromPosts } from "@utils/tags"
+import { sortRssPostsRecentlyPublished, renderPostContent } from "@utils/rss"
+
+import type { APIRoute } from "astro"
+import type { RSSOptions } from "@astrojs/rss"
+
+export async function getStaticPaths() {
+  const posts = await getCollection("posts")
+  const tags = getAlphabeticallySortedTagsFromPosts(posts)
+  return tags.map((tag) => {
+    return {
+      params: {
+        tag,
+      },
+    }
+  })
+}
+
+// See: https://docs.astro.build/en/guides/rss/
+
+export const get: APIRoute = async function get({ params }) {
+  const { tag } = params
+
+  const options: RSSOptions = {
+    title: `${config.RSS_TITLE} tagged ${tag}`,
+    description: config.RSS_DESCRIPTION,
+    site: import.meta.env.SITE,
+    customData: `<language>en-us</language>`,
+    items: [],
+  }
+
+  if (!tag) {
+    return rss(options)
+  }
+
+  const posts = await getCollection("posts")
+  const postsSorted = sortRssPostsRecentlyPublished(posts)
+  const postsForTag = postsSorted.filter((post) => {
+    const { tags = [] } = post.data
+    return tags.includes(tag)
+  })
+  return rss({
+    ...options,
+    items: postsForTag.map((post) => {
+      return {
+        title: post.data.title,
+        pubDate: post.data.publishedAt,
+        description: post.data.description,
+        link: `/posts/${post.slug}/`,
+        content: renderPostContent(post.body),
+      }
+    }),
+  })
+}
